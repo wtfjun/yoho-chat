e156e3f1ea081e89a3b3c8b3a9748dc9828f6b48
cxj <cxj@cxjdeMacBook-Pro.local>
2016-10-07 21:51:45 +0800

    0.0.0

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -8,25 +8,43 @@ io.on('connection', function(_socket) {
 	console.log(_socket.id + ':connection');
 })
 
+var loginErrMsg = {
+	state: 'none',
+	cont: ''
+}; 
+
 exports.showIndex = function(req, res, next) {
 	var users,messages;
 	User.find(function(err, Users) {
 		if(err) return console.error(err);
 		users = Users;
-		console.log(users);
+		// console.log(users);
 	});
-	Message.find(function(err, messages) {
-		if(err) return console.error(err);
-		// console.log(messages);
+	Message.find().sort({'create_at':-1}).exec(function(err, messages) {
 		res.render('index',{
+			loginErrMsg: loginErrMsg,
 			messages: messages,
 			users: users
 		});
 	});
+	
+	loginErrMsg = {
+		state: 'none',
+		cont: ''
+	};  
 }
 
 exports.loginAction = function(req, res, next) {
 	var nickname = req.body.nickname;
+	User.findOne({'nickname': nickname}, function(err, user) {
+		if(err) return console.error(err);
+		if(user) {
+			loginErrMsg = {
+				state: 'display',
+				cont: '该用户已存在'
+			}; 
+		}
+	});
 	var password = req.body.password;
 	
 	var user = new User();
--- public/js/yoho_chat_ui.js
+++ public/js/yoho_chat_ui.js
@@ -1,27 +1,4 @@
-function onClickApplyNickname() {
-	var nickname_edit = $('#nickname-edit');
-	var nickname_error = $("#nickname-error");
-    var name = nickname_edit.val();
-    if ("" == name) {
-	    nickname_error.text("请填写昵称。");
-	    nickname_error.show();
-	    nickname_edit.focus();
-        return;
-    }
-    var name_len = name.length;
-    if (name_len < 4 || name_len > 16) {
-	    nickname_error.text("请填写正确的昵称，应为4到16个字符。");
-	    nickname_error.show();
-        return;
-    }
-    if (name == $.cookie('chat_nickname')) {
-	    nickname_error.text("你本来就叫这个。");
-	    nickname_error.show();
-    }
-    changeNickname(name);
-    // console.log($.cookie('chat_nickname'));
-    $('#login-modal').modal('hide');
-}
+
 
 function changeNickname(_name) {
 	$.cookie('chat_nickname', _name);
@@ -40,7 +17,7 @@ function onClickLogin() {
     setCookie(nickname);
     $('#login-form').submit();
     $('#my-nickname').html('');
-    $('#my-nickname').html($.cookie('chat_nickname'));
+    // $('#my-nickname').html($.cookie('chat_nickname'));
 }
 
 function setCookie(_nicname) {
