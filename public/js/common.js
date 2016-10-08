f8d81f23fa5829325b57d2b74c425951ea49ead4
cxj <cxj@cxjdeMacBook-Pro.local>
2016-10-08 17:10:19 +0800

    1.0.2

--- public/js/common.js
+++ public/js/common.js
@@ -6,5 +6,5 @@ function appendZero(obj) {
 function getLocalHMS() {
     var time = (new Date()).getTime();
     var d = new Date();
-    return ' '+d.getFullYear()+'-' + appendZero(parseInt( d.getMonth()+1 ))+'-' + appendZero(d.getDate())+' ' + appendZero(d.getHours()) + ":" + appendZero(d.getMinutes()) + ":" + appendZero(d.getSeconds());
+    return (' '+d.getFullYear()+'-' + appendZero(parseInt( d.getMonth()+1 ))+'-' + appendZero(d.getDate())+' ' + appendZero(d.getHours()) + ":" + appendZero(d.getMinutes()) + ":" + appendZero(d.getSeconds())).toString();
 }
